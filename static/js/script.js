cdb5123f8b27a83bc332a88e6d920de433754980
UziV05 <uziv05@gmail.com>
2025-09-26 03:14:45 +0100

    First commit - Uzi V Cooperations site

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-// Simple dropdown animation placeholder
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Uzi V Cooperations site loaded successfully.");
+  console.log("Uzi V Cooperations site loaded!");
 });
