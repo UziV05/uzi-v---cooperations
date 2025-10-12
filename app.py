2d494aafc21a0571bb34cc7cb79f073f99d380eb
UziV05 <uziv05@gmail.com>
2025-10-12 16:40:29 +0100

    Initial commit - UV Cooperations website

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home page
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('home.html')
 
-# About page
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
-# Services page
 @app.route('/services')
 def services():
-    return render_template("services.html")
+    return render_template('services.html')
 
-# Blog page
-@app.route('/blog')
-def blog():
-    return render_template("blog.html")
-
-# Careers page
-@app.route('/careers')
-def careers():
-    return render_template("careers.html")
-
-# Contact page
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
-# Portfolio page
 @app.route('/portfolio')
 def portfolio():
-    return render_template("portfolio.html")
+    return render_template('portfolio.html')
 
-# Solutions page
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/careers')
+def careers():
+    return render_template('careers.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/solutions')
 def solutions():
-    return render_template("solutions.html")
+    return render_template('solutions.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/founder')
+def founder():
+    return render_template('founder.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
